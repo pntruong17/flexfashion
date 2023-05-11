afb986ac5d9a71ee70f6252f8a92f4b9508050f1
pntruong17 <pntruong17@gmail.com>
2023-05-11 21:57:37 +0700

    fixed2 10.5

--- pages/api/key.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === "GET") {
-    res.status(200).json({
-      publishableKey: "process.env.STRIPE_SECRET_KEY",
-    });
-  } else {
-    res.status(405).end("Method not allowed");
-  }
-}
